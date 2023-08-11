67574e94e2247342c7893b5a9f885fedad89bf21
sandeep jain <mr.sandeepmcscet@gmail.com>
2023-08-11 13:44:33 +0530

    add react project file

---
+++ react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GraphWidget from './components/GraphWidget';
+function App() {
+  return (
+    <>
+     <main className="container m-5 border border-secondary">
+    <GraphWidget />
+    </main>
+    </>
+  );
+}
+
+export default App;
